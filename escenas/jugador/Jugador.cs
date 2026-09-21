4e8500879173eeda1435a3d551a8eea72bf3b575
MiguelMex <diazmendez2402@gmail.com>
2026-09-21 12:11:09 -0600

    Jugador básico

---
+++ escenas/jugador/Jugador.cs
@@ -0,0 +1,63 @@
+using Godot;
+using System;
+
+public partial class Jugador : CharacterBody2D
+{
+	/**
+	[Export] cumple la función de mostrar un valor del script en el motor grafico de GODOT
+	Speed es la velocidad a la que se moverá el jugador
+	*/
+	[Export] public float Speed = 300.0f;
+
+	/**
+	La velocidad con la que el jugador salta
+	*/
+	[Export]
+	public float JumpVelocity = -400.0f;
+
+	/**
+	Metodo de Godot que se ejecuta durante cada frame, sirve para manejar las fisicas
+	delta: el tiempo que ah pasado desde el último frame, necesario sii trabajamos con aceleración  o para manter consistencia en tiempos incluso si el rendimiento es malo
+	*/
+	public override void _PhysicsProcess(double delta)
+	{
+		//La velocidad con la que se está movindo el jugador en cada frame
+		Vector2 velocity = Velocity;
+
+		// Añadir la gravedad
+		/**
+		IsOnFloor revisa si el nodo se encuentra en una superficie horizontal o si está flotando
+		*/
+		if (!IsOnFloor())
+		{
+			velocity += GetGravity() * (float)delta;
+		}
+
+		// Manejar el salto.
+		if (Input.IsActionJustPressed("jump") && IsOnFloor())
+		{
+			velocity.Y = JumpVelocity;
+		}
+
+		/**
+		Detecta el imput enviado por el jugador, se definen en el motor grafico
+		Solo se está usando izquierda y derecha así que no hay necesidad de asignar las otras dos
+		*/
+		Vector2 direction = Input.GetVector("left", "right","void", "void");
+		//Detecta si hubo algun input
+		if (direction != Vector2.Zero && IsOnFloor())
+		{
+			//Cambio en la velocidad del jugador
+			velocity.X = direction.X * Speed;
+		}
+		else
+		{
+			//Que frene en seco si no hay input
+			velocity.X = 0;
+		}
+
+		//Se asigna el cambio de velocidad
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+}
