8a6103a1e4baa1d46e4fa5feb21d3a65f7d189a4
Miguel O <miguelortiz15r@gmail.com>
2026-09-23 19:44:36 -0600

    first_Commit_maps

---
+++ mapas/tilesets/desierto.tsx
@@ -0,0 +1,107 @@
+<?xml version='1.0' encoding='utf-8'?>
+<tileset version="1.10" tiledversion="1.11.0" name="Desierto_Gekko" tilewidth="16" tileheight="16" tilecount="17" columns="8">
+  <image source="desierto.png" width="128" height="48" />
+  <tile id="0">
+    <properties>
+      <property name="solid" type="bool" value="true" />
+    </properties>
+    <objectgroup draworder="index">
+      <object id="1" x="0" y="0" width="16" height="16" />
+    </objectgroup>
+  </tile>
+  <tile id="1">
+    <properties>
+      <property name="solid" type="bool" value="true" />
+    </properties>
+    <objectgroup draworder="index">
+      <object id="1" x="0" y="0" width="16" height="16" />
+    </objectgroup>
+  </tile>
+  <tile id="2">
+    <properties>
+      <property name="solid" type="bool" value="true" />
+    </properties>
+    <objectgroup draworder="index">
+      <object id="1" x="0" y="0" width="16" height="16" />
+    </objectgroup>
+  </tile>
+  <tile id="3">
+    <properties>
+      <property name="solid" type="bool" value="true" />
+    </properties>
+    <objectgroup draworder="index">
+      <object id="1" x="0" y="0" width="16" height="16" />
+    </objectgroup>
+  </tile>
+  <tile id="4">
+    <properties>
+      <property name="solid" type="bool" value="true" />
+    </properties>
+    <objectgroup draworder="index">
+      <object id="1" x="0" y="0" width="16" height="16" />
+    </objectgroup>
+  </tile>
+  <tile id="5">
+    <properties>
+      <property name="solid" type="bool" value="true" />
+    </properties>
+    <objectgroup draworder="index">
+      <object id="1" x="0" y="0" width="16" height="16" />
+    </objectgroup>
+  </tile>
+  <tile id="6">
+    <properties>
+      <property name="solid" type="bool" value="false" />
+    </properties>
+  </tile>
+  <tile id="7">
+    <properties>
+      <property name="solid" type="bool" value="false" />
+    </properties>
+  </tile>
+  <tile id="8">
+    <properties>
+      <property name="solid" type="bool" value="false" />
+    </properties>
+  </tile>
+  <tile id="9">
+    <properties>
+      <property name="solid" type="bool" value="false" />
+    </properties>
+  </tile>
+  <tile id="10">
+    <properties>
+      <property name="solid" type="bool" value="false" />
+    </properties>
+  </tile>
+  <tile id="11">
+    <properties>
+      <property name="solid" type="bool" value="false" />
+    </properties>
+  </tile>
+  <tile id="12">
+    <properties>
+      <property name="solid" type="bool" value="false" />
+    </properties>
+  </tile>
+  <tile id="13">
+    <properties>
+      <property name="solid" type="bool" value="false" />
+    </properties>
+  </tile>
+  <tile id="14">
+    <properties>
+      <property name="solid" type="bool" value="false" />
+    </properties>
+  </tile>
+  <tile id="15">
+    <properties>
+      <property name="solid" type="bool" value="false" />
+    </properties>
+  </tile>
+  <tile id="16">
+    <properties>
+      <property name="solid" type="bool" value="false" />
+    </properties>
+  </tile>
+</tileset>
