69cb61340658bee549131b78e0a0a55442410de8
MiguelMex <diazmendez2402@gmail.com>
2026-09-24 06:53:02 -0600

    first_sprites

--- escenas/jugador/Jugador.cs
+++ escenas/jugador/Jugador.cs
@@ -3,6 +3,15 @@ using System;
 
 public partial class Jugador : CharacterBody2D
 {
+	public AnimatedSprite2D sprite;
+
+    public override void _Ready()
+    {
+        base._Ready();
+		sprite = GetNode<AnimatedSprite2D>("sprite");
+		_on_no_animation_playing();
+    }
+
 	/**
 	[Export] cumple la función de mostrar un valor del script en el motor grafico de GODOT
 	Speed es la velocidad a la que se moverá el jugador
@@ -14,6 +23,27 @@ public partial class Jugador : CharacterBody2D
 	*/
 	[Export]
 	public float JumpVelocity = -400.0f;
+
+	public void _on_no_animation_playing()
+	{
+		sprite.Play("idle");
+	}
+
+	public void _play_walk_animation(String direction)
+	{
+		switch (direction)
+		{
+			case "left":
+				sprite.Play("left");
+				break;
+			
+			case "right":
+				sprite.Play("right");
+				break;
+
+			default: return;
+		}
+	}
 
 	/**
 	Metodo de Godot que se ejecuta durante cada frame, sirve para manejar las fisicas
@@ -37,6 +67,7 @@ public partial class Jugador : CharacterBody2D
 		if (Input.IsActionJustPressed("jump") && IsOnFloor())
 		{
 			velocity.Y = JumpVelocity;
+			sprite.Play("jump");
 		}
 
 		/**
@@ -44,11 +75,18 @@ public partial class Jugador : CharacterBody2D
 		Solo se está usando izquierda y derecha así que no hay necesidad de asignar las otras dos
 		*/
 		Vector2 direction = Input.GetVector("left", "right","void", "void");
+		if (Input.IsActionPressed("left"))
+		{
+			_play_walk_animation("left");
+		} else if (Input.IsActionPressed("right"))
+		{
+			_play_walk_animation("right");
+		}
 		//Detecta si hubo algun input
-		if (direction != Vector2.Zero && IsOnFloor())
+		if (direction != Vector2.Zero)
 		{
 			//Cambio en la velocidad del jugador
-			velocity.X = direction.X * Speed;
+			velocity.X = direction.X * Speed;			
 		}
 		else
 		{
